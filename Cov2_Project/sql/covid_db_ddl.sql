074796c1cff12221b402e49e2131a45020a09fc9
dowripple <53706893+dowripple@users.noreply.github.com>
2020-04-19 16:14:56 -0500

    adding clean data for tableau, lots of other stuff!

--- Cov2_Project/sql/covid_db_ddl.sql
+++ Cov2_Project/sql/covid_db_ddl.sql
@@ -1,14 +1,36 @@
 
+  -- remove the views if they exist
+  DROP VIEW IF EXISTS staging_county_list;
+  DROP VIEW IF EXISTS staging_state_list;
+  DROP VIEW IF EXISTS staging_cases_list;
+  DROP VIEW IF EXISTS staging_population;
+  DROP VIEW IF EXISTS staging_deaths;
+  DROP VIEW IF EXISTS county_list;
+  DROP VIEW IF EXISTS confirmed_cases_daily;
+
+  -- remove any foreign key references if they exist
+  ALTER TABLE county DROP CONSTRAINT IF EXISTS fk_county_state_id;
+  ALTER TABLE confirmed_cases DROP CONSTRAINT IF EXISTS fk_confirmed_cases_county_fips;
+  ALTER TABLE confirmed_cases DROP CONSTRAINT IF EXISTS fk_confirmed_cases_test_date;
+  ALTER TABLE county_population DROP CONSTRAINT IF EXISTS fk_county_population_county_fips;
+  ALTER TABLE county_deaths DROP CONSTRAINT IF EXISTS fk_county_deaths_county_fips;
+
   -- drop the tables if they exist
   DROP TABLE IF EXISTS staging_confirmed_cases;
   DROP TABLE IF EXISTS county;
   DROP TABLE IF EXISTS states;
+  DROP TABLE IF EXISTS test_dates;
+  DROP TABLE IF EXISTS confirmed_cases;
+  DROP TABLE IF EXISTS staging_county_population;
+  DROP TABLE IF EXISTS county_population;
+  DROP TABLE IF EXISTS staging_county_deaths;
+  DROP TABLE IF EXISTS county_deaths;
   
   -- staging table for confirmed cases
   CREATE TABLE staging_confirmed_cases
   		(county_fips INT,
 		 county_name VARCHAR,
-		 state_name VARCHAR,
+		 state_id VARCHAR,
 		 state_fips INT,
 		 test_date DATE,
 		 confirmed_cases BIGINT
@@ -16,7 +38,7 @@
 
   -- table for county
   CREATE TABLE county
-  		(county_fips INT,
+  		(county_fips INT NOT NULL PRIMARY KEY,
 		 state_fips INT,
 		 county_name VARCHAR,
 		 latitude FLOAT,
@@ -24,9 +46,71 @@
 
   -- table for state
   CREATE TABLE states
-  		(state_fips INT,
+  		(state_fips INT NOT NULL PRIMARY KEY,
 		 state_id VARCHAR);
-		 
 
+  -- create the table for dates
+  CREATE TABLE test_dates
+  		(test_date DATE NOT NULL PRIMARY KEY);
+
+  -- table to store the confirmed case counts by county and date
+  CREATE TABLE confirmed_cases
+  		(test_date DATE NOT NULL,
+		 county_fips INT NOT NULL,
+		 confirmed_cases BIGINT,
+		 CONSTRAINT "pk_confirmed_cases" PRIMARY KEY (
+         		test_date,
+			    county_fips
+		 	)	
+		  );
+
+  -- table for staging
+  CREATE TABLE staging_county_deaths
+  		(county_fips INT,
+		 test_date DATE,
+		 covid_deaths BIGINT);
+		 
+  -- table to store county deaths
+  CREATE TABLE county_deaths
+  		(county_fips INT NOT NULL,
+		 test_date DATE NOT NULL,
+		 covid_deaths BIGINT,
+		 CONSTRAINT "pk_county_deaths" PRIMARY KEY (
+				county_fips,
+				test_date
+		 	)
+		 );
+		 
+  -- table to store the staging population data
+  CREATE TABLE staging_county_population
+  		(county_fips INT,
+		 county_name VARCHAR,
+		 state_id VARCHAR,
+		 population BIGINT);
+
+  -- table for population
+  CREATE TABLE county_population
+  		(county_fips INT NOT NULL PRIMARY KEY,
+		 population BIGINT
+		);
+		
+  -- adding foreign key constraints
+  -- county referencing state
+  ALTER TABLE county ADD CONSTRAINT "fk_county_state_fips" FOREIGN KEY(state_fips)
+	REFERENCES states (state_fips);
+  -- confirmed cases referencing county
+  ALTER TABLE confirmed_cases ADD CONSTRAINT "fk_confirmed_cases_county_fips" FOREIGN KEY (county_fips)
+  	REFERENCES county (county_fips);
+  -- confirmed cases referencing dates
+  ALTER TABLE confirmed_cases ADD CONSTRAINT "fk_confirmed_cases_test_date" FOREIGN KEY (test_date)
+  	REFERENCES test_dates (test_date);
+  -- county population referencing county
+  ALTER TABLE county_population ADD CONSTRAINT "fk_county_population_county_fips" FOREIGN KEY (county_fips)
+  	REFERENCES county (county_fips);
+  -- county deaths referencing county
+  ALTER TABLE county_deaths ADD CONSTRAINT "fk_county_deaths_county_fips" FOREIGN KEY (county_fips)
+  	REFERENCES county (county_fips);
+	
+  
 		
 		
--- Cov2_Project/sql/views.sql
+++ Cov2_Project/sql/views.sql
@@ -1,7 +1,11 @@
 
   DROP VIEW IF EXISTS staging_county_list;
   DROP VIEW IF EXISTS staging_state_list;
+  DROP VIEW IF EXISTS staging_cases_list;
+  DROP VIEW IF EXISTS staging_population;
+  DROP VIEW IF EXISTS staging_deaths;
   DROP VIEW IF EXISTS county_list;
+  DROP VIEW IF EXISTS confirmed_cases_daily;
 
   -- view to use when building county table
   CREATE VIEW staging_county_list
@@ -18,12 +22,38 @@ GROUP BY county_fips,
   CREATE VIEW staging_state_list
      AS
   SELECT state_fips,
-  		 state_name
+  		 state_id
     FROM staging_confirmed_cases
 GROUP BY state_fips,
-		 state_name;
-		 
+		 state_id;
 
+  --view to populate the confirmed_cases view
+  CREATE VIEW staging_cases_list
+      AS
+  SELECT county_fips,
+  		 test_date,
+		 confirmed_cases
+    FROM staging_confirmed_cases
+   WHERE county_fips <> 0;
+
+  -- view to populate population!
+  CREATE VIEW staging_population
+      AS
+  SELECT county_fips,
+  		 population
+    FROM staging_county_population
+   WHERE county_fips <> 0;
+
+  -- view to populate county deaths
+  CREATE VIEW staging_deaths
+      AS
+  SELECT county_fips,
+  		 test_date,
+		 covid_deaths
+    FROM staging_county_deaths
+   WHERE county_fips <> 0;
+   
+  -- county list (used for geo-coding)
   CREATE VIEW county_list
       AS
   SELECT cty.county_fips,
@@ -34,3 +64,44 @@ GROUP BY state_fips,
 		 cty.longitude
     FROM county cty INNER JOIN states s
 	  ON cty.state_fips = s.state_fips;
+	  
+  -- plottin' views!
+  CREATE VIEW confirmed_cases_daily
+      AS
+    WITH county_detail
+	  AS
+	   (
+		  SELECT county_fips,
+				 test_date,
+				 RANK() OVER(PARTITION BY county_fips ORDER BY test_date) AS date_rank,
+				 confirmed_cases
+			FROM confirmed_cases
+	   )
+  SELECT cd1.county_fips,
+  		 c.county_name,
+		 c.latitude,
+		 c.longitude,
+		 s.state_id,
+		 cp.population,
+		 cd1.test_date,
+		 cd1.confirmed_cases,
+		 cd.covid_deaths,
+  		 cd2.test_date AS previous_date,
+		 cd2.confirmed_cases AS previous_confirmed_cases,
+		 (cd1.confirmed_cases - cd2.confirmed_cases) AS daily_change,
+		 CASE WHEN cd2.confirmed_cases <> 0 
+		 	THEN (cd1.confirmed_cases - cd2.confirmed_cases)/(cd2.confirmed_cases*1.0)
+			ELSE NULL
+		 END AS daily_change_percent
+    FROM county_detail cd1 INNER JOIN county_detail cd2
+	  ON cd1.county_fips = cd2.county_fips
+	 AND cd1.date_rank = cd2.date_rank + 1 INNER JOIN county c
+	  ON cd1.county_fips = c.county_fips INNER JOIN states s
+	  ON c.state_fips = s.state_fips LEFT OUTER JOIN county_population cp
+	  ON cd1.county_fips = cp.county_fips LEFT OUTER JOIN county_deaths cd
+	  ON cd1.county_fips = cd.county_fips
+	 AND cd1.test_date = cd.test_date
+   WHERE cd1.test_date <= '2020-04-16'
+     AND c.county_name NOT LIKE '%Unallocated/Probable%'
+	  
+  
